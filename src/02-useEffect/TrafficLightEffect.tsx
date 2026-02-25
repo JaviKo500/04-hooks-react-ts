1d835cb0d3ae733f834cc9d377bf49318959fcbf
JaViKo500 <javikogutierrez64@gmail.com>
2026-02-24 23:51:49 -0500

    feat: add component with useEffect hook

---
+++ src/02-useEffect/TrafficLightEffect.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+const colors: Record<string, string> = {
+  red: 'bg-red-500 animate-pulse',
+  yellow: 'bg-yellow-500 animate-pulse',
+  green: 'bg-green-500 animate-pulse',
+}
+
+// type TrafficLightColors = 'red' | 'yellow' | 'green';
+type TrafficLightColors = keyof typeof colors;
+
+export const TrafficLightEffect = () => {
+
+  const [light, setLight] = useState<TrafficLightColors>('red');
+  const [countDown, setCountDown] = useState<number>(10);
+
+  useEffect( () => {
+    if (countDown === 0)  return;
+    const intervalId = setInterval( () => {
+      setCountDown( countDown - 1 );
+    }, 1000 );
+    return () => clearInterval( intervalId );
+  }, [countDown]);
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
+      <div className="flex flex-col items-center space-y-8">
+        <h1 className="text-white text-3xl font-hint">Traffic Light Effect</h1>
+        <h2 className="text-white text-xl">CountDown: {countDown}</h2>
+
+        <div className={`w-32 h-32 ${ light === 'red' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+        <div className={`w-32 h-32 ${light === 'yellow' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+        <div className={`w-32 h-32 ${light === 'green' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+      </div>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 // import { HooksApp } from './HooksApp';
-import { TrafficLight } from './01-useState/TrafficLight';
+// import { TrafficLight } from './01-useState/TrafficLight';
+import { TrafficLightEffect } from './02-useEffect/TrafficLightEffect';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <HooksApp/> */}
-    <TrafficLight/>
+    {/* <TrafficLight/> */}
+    <TrafficLightEffect/>
   </StrictMode>,
 )
