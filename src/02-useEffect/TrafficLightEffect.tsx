3c35466506ccb5a5c500ac605f3fd3193373b170
JaViKo500 <javikogutierrez64@gmail.com>
2026-02-25 00:17:54 -0500

    feat: add loader and update colors traffic

--- src/02-useEffect/TrafficLightEffect.tsx
+++ src/02-useEffect/TrafficLightEffect.tsx
@@ -14,6 +14,7 @@ export const TrafficLightEffect = () => {
   const [light, setLight] = useState<TrafficLightColors>('red');
   const [countDown, setCountDown] = useState<number>(10);
 
+  // * countDown effect
   useEffect( () => {
     if (countDown === 0)  return;
     const intervalId = setInterval( () => {
@@ -21,12 +22,35 @@ export const TrafficLightEffect = () => {
     }, 1000 );
     return () => clearInterval( intervalId );
   }, [countDown]);
+
+  if ( countDown  === 0 ) {
+    switch (light) {
+      case 'red':
+        setCountDown(10);
+        setLight('green');  
+        break;
+      case 'yellow':
+        setCountDown(10);
+        setLight('red');
+        break;
+      case 'green':
+        setCountDown(2);
+        setLight('yellow');
+        break;
+    }
+  }
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
       <div className="flex flex-col items-center space-y-8">
         <h1 className="text-white text-3xl font-hint">Traffic Light Effect</h1>
         <h2 className="text-white text-xl">CountDown: {countDown}</h2>
-
+        <div className="w-64 bg-gray-700 rounded-full h-2">
+          <div className="bg-blue-500 h-2 rounded-full transition-all duration-100 is-linear"
+            style={{
+              width: `${ (countDown/10) * 100 }%`
+            }}
+          ></div>
+        </div>
         <div className={`w-32 h-32 ${ light === 'red' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
         <div className={`w-32 h-32 ${light === 'yellow' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
         <div className={`w-32 h-32 ${light === 'green' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
