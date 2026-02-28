30eda2e427f9b0164494923292d085328a496026
JaViKo500 <javikogutierrez64@gmail.com>
2026-02-27 21:44:25 -0500

    feat: create taskReducer

--- src/05-useReducer/TaskApp.tsx
+++ src/05-useReducer/TaskApp.tsx
@@ -6,12 +6,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-
-interface Todo {
-  id: number;
-  text: string;
-  completed: boolean;
-}
+import type { Todo } from './reducer/tasksReducer';
 
 export const TasksApp = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
