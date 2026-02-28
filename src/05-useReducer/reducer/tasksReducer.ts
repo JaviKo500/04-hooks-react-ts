7449ac6c8570b84f80c796f25e19c8e1689c76d5
JaViKo500 <javikogutierrez64@gmail.com>
2026-02-27 22:16:10 -0500

    feat: add actions in taskReducer

--- src/05-useReducer/reducer/tasksReducer.ts
+++ src/05-useReducer/reducer/tasksReducer.ts
@@ -17,5 +17,53 @@ export type TaskAction =
 | { type: 'DELETE_TODO', payload: number}
 
 export const tasksReducer = ( state: TaskState, action: TaskAction ): TaskState => {
-  return state;
+  const { type, payload } = action;
+  switch (type) {
+    case 'ADD_TODO': { 
+      if ( !payload.trim().length ) return state;
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: payload,
+        completed: false
+      };
+      const todos = [ newTodo, ...state.todos ];
+      return {
+        ...state,
+        todos,
+        length: todos.length,
+        pending: state.pending + 1,
+      }; 
+    }
+    case 'TOGGLE_TODO':{
+      const updatedTodos = state.todos.map( todo => {
+        if ( todo.id === payload ) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      });
+      const pending = updatedTodos.filter( todo => !todo.completed ).length;
+      return {
+        ...state,
+        todos: updatedTodos,
+        pending,
+        completed: updatedTodos.length - pending,
+      };
+    }
+    case 'DELETE_TODO':{
+      const filteredTodos = state.todos.filter( todo => todo.id !== payload );
+      const pending = filteredTodos.filter( todo => !todo.completed ).length;
+      return {
+        ...state,
+        todos: filteredTodos,
+        length: filteredTodos.length,
+        pending,
+        completed: filteredTodos.length - pending,
+      };
+    }
+    default:
+      return state;
+  }
 }
